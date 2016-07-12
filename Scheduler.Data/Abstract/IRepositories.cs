b355c2a150d8500b37b75c72f524d414c173f009
jmorris <jmorris@CP-JEFF03>
2016-07-12 16:40:25 -0500

    finished tutorial with working api

---
+++ Scheduler.Data/Abstract/IRepositories.cs
@@ -0,0 +1,16 @@
+﻿using Scheduler.Model;
+using Scheduler.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Scheduler.Data.Abstract
+{
+    public interface IScheduleRepository : IEntityBaseRepository<Schedule> { }
+
+    public interface IUserRepository : IEntityBaseRepository<User> { }
+
+    public interface IAttendeeRepository : IEntityBaseRepository<Attendee> { }
+}
---
+++ Scheduler.Data/Repositories/UserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Scheduler.Data.Abstract;
+using Scheduler.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scheduler.Data.Repositories
+{
+    public class UserRepository : EntityBaseRepository<User>, IUserRepository
+    {
+        public UserRepository(SchedulerContext context)
+            : base(context)
+        { }
+    }
+}
